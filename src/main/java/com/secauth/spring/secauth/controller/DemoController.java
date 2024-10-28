407947c76e9f6abe6916b50614d19183c7a458bb
mthalima <matheus.ecomp@gmail.com>
2024-10-28 16:25:40 -0300

    many security tests

--- src/main/java/com/secauth/spring/secauth/controller/DemoController.java
+++ src/main/java/com/secauth/spring/secauth/controller/DemoController.java
@@ -7,8 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoController {
 
+
+    //get mapping para /home
     @GetMapping("/")
     public String showHome(){
         return "home";
+    }
+
+
+    //add request mapping para "/gerentes"
+
+    @GetMapping("/gerentes")
+    public String showGerentes(){
+        return "gerentes";
     }
 }
